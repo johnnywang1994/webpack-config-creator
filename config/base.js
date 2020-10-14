ae611c9d0222c074e50c333869b8e6a84ecc0660
Johnny Wang <johnny29621189@kimo.com>
2020-10-14 12:24:17 +0800

    UPD: add .mjs to resolve extensions

--- config/base.js
+++ config/base.js
@@ -17,7 +17,7 @@ module.exports = (options) => {
       splitChunks: { chunks: 'all' },
     },
     resolve: {
-      extensions: ['.js', '.vue', '.json', '.scss', '.svelte'],
+      extensions: ['.mjs', '.js', '.vue', '.json', '.scss', '.svelte'],
       alias: {
         '@': resolve('./src'),
       }
